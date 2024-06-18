f58be5591fabeaa7b1daacb936296424603835a3
sandipkunwar123 <sandipkunwar123p@gmail.comgit config --global user.name>
2024-06-18 18:17:42 +0545

    New file added

--- src/app/counter.tsx
+++ src/app/counter.tsx
@@ -1,29 +1,23 @@
 // state
 // Mutuable
 // synchronous
-import Button from "../components/button"
-import { useState } from "react"
+import { Button } from "../components/ui/button";
+import { useState } from "react";
 
-
-
-export default function Counter(){
-     // let count =0
-     const [count,setCount] = useState(0)
-     const handleAdd = ()=>{
-         setCount(count +1)
-     }
-     const handleSubtract = ()=>{
-         setCount(count -1)
-}
-return(
+export default function Counter() {
+  // let count =0
+  const [count, setCount] = useState(0);
+  const handleAdd = () => {
+    setCount(count + 1);
+  };
+  const handleSubtract = () => {
+    setCount(count - 1);
+  };
+  return (
     <div>
-    <h1>{count}</h1>
-    <Button title="Subtract" onClick={handleSubtract}/>
-    <Button title="Add" onClick={handleAdd}/>
-</div>
-)
-
-    }
-   
-       
-    
+      <h1>{count}</h1>
+      <Button onClick={handleSubtract}>Subtract</Button>
+      <Button onClick={handleAdd}>Added</Button>
+    </div>
+  );
+}
