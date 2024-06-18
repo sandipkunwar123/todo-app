19766d4471af40810c381f432327079abf85cb1d
sandipkunwar123 <sandipkunwar123p@gmail.comgit config --global user.name>
2024-06-18 07:28:51 +0545

    Search file updated

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -2,22 +2,19 @@
 //   return <input type="text" className="border rounded py-2 px-3" />;
 // }
 
+export default function Search(props) {
+  const { search, handleChange } = props;
 
-import {useState} from "react"
-
-
-export default function Search() {
-
-  const [search,setSearch] =useState('')
-  const handleChange = (e)=>{
-    console.log(e.target.value)
-    setSearch(e.target.value)
-
-  }
-  return(
+  return (
     <div>
-      <input type="text"  className="border rounded py -2 px-3" value={search} onChange={handleChange}/>
+      <input
+        type="text"
+        className="border rounded py -2 px-3"
+        value={search}
+        onChange={handleChange}
+        placeholder="Search"
+      />
       <h1>Search Result:{search}</h1>
     </div>
-  )
+  );
 }
